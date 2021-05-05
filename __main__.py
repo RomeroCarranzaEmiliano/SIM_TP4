3a43dabc9ef22ff45fdad0ad8570751ee8de4b28
Emiliano Romero Carranza <52361057+RomeroCarranzaEmiliano@users.noreply.github.com>
2021-05-05 00:46:22 -0300

    Estructura de la app, servidor ws, diccionario de eventos

---
+++ __main__.py
@@ -0,0 +1,6 @@
+"""
+    __main__.py
+
+    Desde este archivo se inicia la api
+
+"""
---
+++ api/events.py
@@ -0,0 +1,30 @@
+"""
+    events.py
+
+    Diccionario de eventos apuntando a su respectiva función.
+
+    Para agregar una nueva funcionalidad, se debe crear una función que haga lo pertinente y luego
+    agregar esa función al diccionario events = {}
+
+"""
+
+# IMPORTS #########################################################################################
+###################################################################################################
+
+
+# Funciones
+# -------------------------------------------------------------------------------------------------
+def ws_test():
+    """
+        retorna un "OK", esta definición de evento es redundante, es solo para ilustrar
+    """
+    return "OK"
+
+
+# Diccionario de eventos
+# -------------------------------------------------------------------------------------------------
+events = {
+    "ws-test": ws_test,
+}
+
+
